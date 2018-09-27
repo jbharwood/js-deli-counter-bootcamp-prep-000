e6261d0bd4a7b6299ebb03aabe753dacaa04a114
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-27 20:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 function takeANumber(line) {
   var number = 0
-  for (var i = 0; i < line.length; i++) {
+  for (var i = 0; i <= line.length; i++) {
     number++
     return `Now Serving: ${number}`
   }
